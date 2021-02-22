2a92f08f5242883c69bcf0d85a81432ba6fd6ae7
Pezhman <70278866+pezhman1407@users.noreply.github.com>
2021-02-23 02:01:14 +0330

    Add files via upload

---
+++ hud_esx/prot-hud/prot-hud/config.lua
@@ -0,0 +1,23 @@
+--[[    ============================================  ]]--
+--[[        Credit :  Pezhman With Love               ]]--    
+--[[        Discord : Pezhman#2494                    ]]--
+--[[        If Any Problem Dm Me                      ]]--
+--[[    ============================================  ]]--  
+
+EVHud = {}
+
+EVHud.Show = false
+
+EVHud.Money = {}
+EVHud.Money.ShowConstant = false -- Show money constantly
+
+EVHud.Radar = {}
+EVHud.Radar.NoRadarVehicles = {
+    "bmx",
+    "cruiser",
+    "fixter",
+    "scorcher",
+    "tribike",
+    "tribike2",
+    "tribike3",
+}
